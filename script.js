093d3da22ad679a589fbb762f542536211c21053
rizwanfrnt-end <86659093+rizwanfrnt-end@users.noreply.github.com>
2021-07-09 12:47:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+//challange 1//
+/*function ageInDays() {
+    let name = "rizwan"
+    var yourName = prompt("pls enter your name");
+    // switch(yourName){
+    //     case "rizwan":
+    //      alert(Text = "nice name");
+    //     break;
+    //     default:
+    //         alert(Text= "not rizwan");
+    // }
+
+    if(yourName == 'rizwan'){
+        document.getElementById("rreal").innerHTML = "welcome";
+        
+    }else{
+        alert(text='aaaaa')
+    }
+
+    
+
+}
+
+function resset(){
+    document.getElementById("real").remove();
+    document.getElementById("rreal").remove();
+}
+
+let p = prompt("enter how many stars");
+let i ='*';
+for(let i=1; i<p.length; i++){
+    console.log('*'.repeat(i));
+   
+}*/
+
+//enter the number and get given number stars//
+
+/*let my = prompt("enter stars");
+//document.write(my);//
+let i = "*";
+for(let i =0; i<=my; i++){
+    console.log("*".repeat(i));
+}
+
+//multiplication table//
+const number = prompt("enter an number")
+
+function multi(){
+   let numOne= document.getElementById("flab").value;
+
+    let numTwo=document.getElementById("slab").value;
+
+   console.log(numOne*numTwo)*/
+  
+   
+
+   //Remove duplicate from element//
+/*let arr = ["mango","orange","apple","chickooo","hanuman","apple","mango"];
+
+arr = arr.filter((value,index,arr)=>arr.indexOf(value)==index);
+arr.forEach(data=>console.log(data));
+
+var a=prompt("enter your number") ;
+var b=prompt("enter your number");
+var c;
+for(var i=1; i<=b; i++){
+    c = a*i;
+    document.write(' '+ a + "*" +i+ "=" +c);
+}*/
+
